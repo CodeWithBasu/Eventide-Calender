37a12a39178f621ebd5828031520f50448dfb277
Basudev Moharana <basudevmuna111@gmail.com>
2026-06-27 21:56:45 +0530

    feat: add simulated loading delay to show loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import { Suspense } from "react"
 import DesignEventsCalendar from "@/components/design-events-calendar"
 
-export default function Page() {
+export default async function Page() {
+  // Artificial delay so you can see the new loading animation!
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
   return (
     <Suspense fallback={null}>
       <DesignEventsCalendar />
